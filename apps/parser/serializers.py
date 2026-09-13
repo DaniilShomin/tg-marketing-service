4558809f80683cf7443f79b3039668660eb4ed30
lisa <lisavetabelova@gmail.com>
2026-09-13 13:20:46 +0300

    add calculate_er() to Post model, add unit tests, integrate new metrics into PostSerializer

--- apps/parser/serializers.py
+++ apps/parser/serializers.py
@@ -13,6 +13,7 @@ class PostSerializer:
     @classmethod
     def get_post_data(cls, post: Post) -> dict:
         breakdown = post.get_reactions_breakdown()
+        er = post.calculate_er()
 
         """
         Словарь пропсов для фронтенда (Inertia.js):
@@ -91,6 +92,7 @@ class PostSerializer:
             "is_pinned": post.is_pinned,
             "media_type": post.media_type,
             "permalink": post.permalink,
+            "er": er,
             "reactions": {
                 "total": breakdown["total"],
                 "details": breakdown["details"],
